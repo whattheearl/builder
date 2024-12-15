573dc091026a954fbbb8f465c80fa9daaa01a279
jonathan earl <jonathan.earl@tylertech.com>
2024-12-15 15:44:01 -0800

    more verbose

--- src/lib/run.ts
+++ src/lib/run.ts
@@ -1,18 +1,86 @@
-import { $ } from 'bun';
-import { type Build, updateBuildOutput } from '../db/builds';
+import { $ } from "bun";
+import { type Build, updateBuild } from "../db/builds";
 
 export async function run(build: Build) {
-    let output = "";
-    output += await $`rm -rf ${build.build_path}`.text();
-    updateBuildOutput(build.id, output);
+    let output = `Running build ${build.name}:${build.id}`;
+    console.log(output);
+    build.output = output;
+    updateBuild(build);
 
-    output += await $`git clone ${build.git_repository} ${build.build_path}`.text();
-    updateBuildOutput(build.id, output);
+    {
+        console.log(`Cleaning up ${build.build_path}`);
+        const { stdout, stderr, exitCode } = await $`rm -rf ${build.build_path}`
+            .quiet()
+            .nothrow();
+        if (exitCode != 0) {
+            console.error("failed to remove build folder");
+            build.output += stderr.toString();
+            return;
+        }
+        console.log(stdout.toString());
+        build.output += output;
+        updateBuild(build);
+    }
 
-    output += await $`make -C ${build.build_path} build; make -C ${build.build_path} publish`.text();
-    updateBuildOutput(build.id, output);
+    {
+        console.log(
+            `Cloning repository ${build.git_repository} to ${build.build_path}`,
+        );
+        const { stdout, stderr, exitCode } =
+            await $`git clone ${build.git_repository} ${build.build_path}`
+                .quiet()
+                .nothrow();
+        if (exitCode != 0) {
+            console.error("failed to clone repository");
+            build.output += stderr.toString();
+            return;
+        }
+        console.log(stdout.toString());
+        build.output += output;
+        updateBuild(build);
+    }
 
-    output += await $`rm -rf ${build.build_path}`.text();
-    updateBuildOutput(build.id, output);
+    {
+        console.log(`Building ${build.name}`);
+        const { stdout, stderr, exitCode } =
+            await $`make -C ${build.build_path} build;`.quiet().nothrow();
+        if (exitCode != 0) {
+            console.error(`Failed to build ${build.name}`);
+            build.output += stderr.toString();
+            return;
+        }
+        console.log(stdout.toString());
+        build.output += output;
+        updateBuild(build);
+    }
+
+    {
+        console.log(`Publishing ${build.name}`);
+        const { stdout, stderr, exitCode } =
+            await $`make -C ${build.build_path} publish`.quiet().nothrow();
+        if (exitCode != 0) {
+            console.error("Failed to publish");
+            build.output += stderr.toString();
+            return;
+        }
+        console.log(stdout.toString());
+        build.output += output;
+        updateBuild(build);
+    }
+
+    {
+        console.log(`Cleaning up ${build.build_path}`);
+        console.log(`rm -rf ${build.build_path}`);
+        const { stdout, stderr, exitCode } = await $`rm -rf ${build.build_path}`
+            .quiet()
+            .nothrow();
+        if (exitCode != 0) {
+            console.error("Failed to clean up build files");
+            build.output += stderr.toString();
+            return;
+        }
+        console.log(stdout.toString());
+        build.output += output;
+        updateBuild(build);
+    }
 }
-
